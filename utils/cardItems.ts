d08316b5c9dc05587b48a7a1d57b4934c34e485c
muhammed <www.savad34@gmail.com>
2023-04-30 16:50:47 +0530

    change about section

--- utils/cardItems.ts
+++ utils/cardItems.ts
@@ -1,21 +1,21 @@
 export const cardContent = [
   {
     title: "Netflix",
-    description: "The world's most popular movie-making platform.",
+    description: "The world's most popular streaming Platform.",
     url: "images/netflix.png",
     webUrl: "https://muhammedsavadtp.github.io/netflix-clone-app/",
     git: "https://github.com/muhammedsavadtp/netflix-clone-app",
   },
   {
     title: "olx",
-    description: "The world's most popular open source software.",
+    description: "The world's leading classifieds platform",
     url: "images/olx.png",
     webUrl: "https://muhammedsavadtp.github.io/olx-clone-app/",
     git: "https://github.com/muhammedsavadtp/olx-clone-app",
   },
   {
     title: "shopping cart",
-    description: "The world's most popular shopping cart.",
+    description: "India's leading E-commerce marketplaces",
     url: "images/cartcastle.png",
     webUrl: "https://cartcastle.vercel.app/",
     git: "https://github.com/muhammedsavadtp/cartcastle",
