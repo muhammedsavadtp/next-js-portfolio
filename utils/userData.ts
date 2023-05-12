1c7e16f0181a8628a5aa7f5d0c819fa6aa69c7cb
muhammed <www.savad34@gmail.com>
2023-05-12 15:22:18 +0530

    change : about me section and cv pdf

--- utils/userData.ts
+++ utils/userData.ts
@@ -6,12 +6,8 @@ const data = {
   stack: "full stack developer",
   greeting: "Hey!",
   description: `  I focus on developing user-friendly web applications that meet the client's requirements, with attention to detail, scalability, and performance.`,
-  aboutMe: `I am a full-stack developer based in Kerala, India. With a strong understanding of the MERN stack, I bring a unique perspective to the world of web development. Despite not having a formal IT education, my passion for technology and eagerness to learn has led me to become proficient in the latest web development tools and technologies.
-
-With a deep understanding of MongoDB, Express.js, React, and Node.js, I am capable of building scalable and robust full-stack web applications. My experience in self-learning has honed my critical thinking and problem-solving skills, enabling me to tackle complex coding challenges with ease. I am highly motivated to work with diverse teams and collaborate to build innovative solutions that drive business growth.
-
-I am excited about the opportunity to work with like-minded professionals in a dynamic environment that encourages creativity and innovation. `,
-  title: ["UI/UX designer", "front end developer", "backend developer"],
+  aboutMe: `self-taught Full Stack Developer with a specialization in the MERN stack. I possess a strong understanding of the latest technologies and am passionate about building scalable web applications. I have designed user interfaces using Figma and utilized frameworks like Next.js to optimize website performance. I have excellent communication skills and enjoy working collaboratively with a team to deliver high-quality projects. `,
+  title: ["front end developer", "backend developer", "UI/UX designer"],
   homePageImage: "images/bmoji.png",
   aboutPageImage: "images/mypic.jpg",
 };
