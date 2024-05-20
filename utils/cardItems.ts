37f23706d040a58f62d5995a572fda23335fd9ab
muhammed <www.savad34@gmail.com>
2024-05-20 10:33:34 +0530

    updated portfolio

--- utils/cardItems.ts
+++ utils/cardItems.ts
@@ -1,18 +1,18 @@
 export const cardContent = [
-  {
-    title: "Netflix",
-    description: "The world's most popular streaming Platform.",
-    url: "images/netflix.png",
-    webUrl: "https://muhammedsavadtp.github.io/netflix-clone-app/",
-    git: "https://github.com/muhammedsavadtp/netflix-clone-app",
-  },
-  {
-    title: "olx",
-    description: "The world's leading classifieds platform",
-    url: "images/olx.png",
-    webUrl: "https://muhammedsavadtp.github.io/olx-clone-app/",
-    git: "https://github.com/muhammedsavadtp/olx-clone-app",
-  },
+  // {
+  //   title: "Netflix",
+  //   description: "The world's most popular streaming Platform.",
+  //   url: "images/netflix.png",
+  //   webUrl: "https://muhammedsavadtp.github.io/netflix-clone-app/",
+  //   git: "https://github.com/muhammedsavadtp/netflix-clone-app",
+  // },
+  // {
+  //   title: "olx",
+  //   description: "The world's leading classifieds platform",
+  //   url: "images/olx.png",
+  //   webUrl: "https://muhammedsavadtp.github.io/olx-clone-app/",
+  //   git: "https://github.com/muhammedsavadtp/olx-clone-app",
+  // },
   {
     title: "shopping cart",
     description: "India's leading E-commerce marketplaces",
